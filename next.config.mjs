ac0b3f1cc329e807a1fd70e2ad4f3566a2003cfc
soumik9870 <soumiksaha827@gmail.com>
2024-05-09 22:38:14 +0530

    Updated dependencies and UI

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-
+images: {
+    remotepatterns: [
+        {
+            protocol: 'https',
+            hostname: 'res.cloudinary.com',
+            port: ''
+        }
+    ]
+};
 export default nextConfig;
